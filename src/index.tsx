d53fc013433488da0e6e5fe12a890f8d0dc3c911
1998YUKI0331 <dkenl0110@dongguk.edu>
2022-06-09 19:33:48 +0900

    react-beautiful-library with TS

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,11 @@
 import ReactDOM from "react-dom/client";
-import App from "./App";
+import App2 from "./dnd-with-library";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
 root.render(
   <>
-    <App />
+    <App2 />
   </>
 );
